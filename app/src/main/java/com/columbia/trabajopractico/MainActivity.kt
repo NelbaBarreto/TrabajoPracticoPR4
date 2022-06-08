f1d20927d96b1243433b838e9235ba7e6710199a
Nelba Barreto <barretonelba@gmail.com>
2022-06-07 22:07:31 -0400

    Terminar lógica de pantallla de registro

--- app/src/main/java/com/columbia/trabajopractico/MainActivity.kt
+++ app/src/main/java/com/columbia/trabajopractico/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.columbia.trabajopractico
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var bHacerPedido: Button
+    private lateinit var bRegistro: Button
+
+    private lateinit var usuario: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bRegistro = findViewById(R.id.bRegistrarse)
+        bRegistro.setOnClickListener(this)
+    }
+
+   override fun onClick(v: View?) {
+        if (v?.id == bRegistro.id){
+            lanzarRegistro()
+        }
+    }
+
+   private val launcherRegistro =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                (it.data?.extras?.getString("usuario") ?: "").also { usuario = it }
+                Toast.makeText(this, R.string.registro_exitoso, Toast.LENGTH_LONG).show()
+            } else if (it.resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, R.string.registro_cancelado, Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun lanzarRegistro() {
+        val intent = Intent(this, Registro::class.java)
+        launcherRegistro.launch(intent)
     }
 }
--- app/src/main/java/com/columbia/trabajopractico/Registro.kt
+++ app/src/main/java/com/columbia/trabajopractico/Registro.kt
@@ -1,11 +1,113 @@
+@file:Suppress("DEPRECATION")
+
 package com.columbia.trabajopractico
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Context
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
-class Registro : AppCompatActivity() {
+class Registro : AppCompatActivity(), View.OnClickListener  {
+    //Class variables
+    private lateinit var username: String
+    private lateinit var password: String
+    private lateinit var repeatPassword: String
+
+    private lateinit var bAceptar: Button
+    private lateinit var bCancelar: Button
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var repeatPasswordEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registro)
+
+        bAceptar = findViewById(R.id.bAceptar)
+        bAceptar.setOnClickListener(this)
+
+        bCancelar = findViewById(R.id.bCancelar)
+        bCancelar.setOnClickListener(this)
+
+        usernameEditText = findViewById(R.id.etUsuario)
+        passwordEditText = findViewById(R.id.etPassword)
+        repeatPasswordEditText = findViewById(R.id.etRepeatPassword)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == bAceptar.id) {
+            aceptar()
+        } else {
+            cancelar()
+        }
+    }
+
+    private fun aceptar() {
+        username = usernameEditText.text.toString()
+        password = passwordEditText.text.toString()
+        repeatPassword = repeatPasswordEditText.text.toString()
+
+        var fieldBlankError: String = getString(R.string.err_fld_blank)
+        var passwordsDoNotMatchError: String = getString(R.string.err_pass_do_not_match)
+
+        if (username?.isNullOrEmpty()) {
+            usernameEditText.error = fieldBlankError
+            return
+        } else if (password?.isNullOrEmpty()) {
+            passwordEditText.error = fieldBlankError
+            return
+        } else if (repeatPassword?.isNullOrEmpty()) {
+            repeatPasswordEditText.error = fieldBlankError
+            return
+        } else if (!password.equals(repeatPassword)) {
+            repeatPasswordEditText.error = passwordsDoNotMatchError
+            return
+        } else {
+            val proceso = ProcesoRegistro(this)
+            proceso.execute(username)
+        }
+    }
+
+    private fun cancelar() {
+        val caller = intent
+        setResult(Activity.RESULT_CANCELED, caller)
+        finish()
+    }
+
+    inner class ProcesoRegistro(private var contexto: Context) :
+        AsyncTask<String, String, Long>() {
+        private lateinit var progreso: ProgressDialog
+
+        override fun onPreExecute() {
+            progreso = ProgressDialog(contexto)
+            progreso.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+            progreso.setMessage(getString(R.string.procesando_registro))
+            progreso.setCancelable(true)
+            progreso.progress = 0
+            progreso.show()
+        }
+
+        override fun doInBackground(vararg params: String?): Long {
+            progreso.progress = 50
+            SystemClock.sleep(2000)
+            progreso.progress = 100
+            intent.putExtra("usuario", params[0])
+            return 1L
+        }
+
+        override fun onPostExecute(result: Long?) {
+            super.onPostExecute(result)
+            progreso.dismiss()
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
     }
 }
